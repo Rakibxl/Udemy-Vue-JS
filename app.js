2fc96cbd240eb0731b4c4cf3bccd2beedf327c9a
Rakibxl <Administrator@DESKTOP-H7T44MN>
2020-08-18 14:42:48 +0200

    Vue methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#app',
     data:{
-        title: 'Becoming a vue ninja'
+        title: 'Becoming a vue ninja',
+        name: 'Rakib'
+    },
+    methods:{
+        greet(time){            
+            return `Hello and good ${time} ${this.name}`
+        }
     }
 })
