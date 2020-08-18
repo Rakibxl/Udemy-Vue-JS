9d546b7b9af49cbb3fe8ec938eecb9525672ac1d
Rakibxl <Administrator@DESKTOP-H7T44MN>
2020-08-18 15:41:44 +0200

    Vue Events

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ new Vue({
     el: '#app',
     data:{
         title: 'Becoming a vue ninja',
-        name: 'Rakib',
-        url:'http:/wwww.youtube.com',
-        classes: ['one', 'two']
+        wage: 10
     },
     methods:{
-        greet(time){            
-            return `Hello and good ${time} ${this.name}`
-        }
-    }
+            changeWage(amount){
+                this.wage += amount
+            }
+        }   
 })
