938070e730f04bb0f3f4581157a49ed194c0beb5
Rakibxl <Administrator@DESKTOP-H7T44MN>
2020-08-19 23:20:45 +0200

    Added Looping with v-for

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ new Vue({
     el: '#app',
     data:{
         title: 'Becoming a vue ninja',
-        showName: false,
-        showAge: true
+        items: ['Mushroom', 'Green Shells', 'Red Shells', 'Banana', 'Star'],
+        ninjas: [
+            {name: 'Crystal', age:25, belt: 'Black'},
+            {name: 'Ryu', age:30, belt: 'Brown'},
+            {name: 'Rakib', age:31, belt: 'Orange'}
+        ]        
     },
     methods:{
         toggleName(){
